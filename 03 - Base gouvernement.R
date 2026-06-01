f8dea34ca2b3f665f4ed1812cf62cb26ca2fcf8c
LouisNURDIN <louis.nurdin@etu-iepg.fr>
2026-06-01 16:50:32 +0200

    Add files via upload

---
+++ 03 - Base gouvernement.R
@@ -0,0 +1,4 @@
+#Base avec données gouvernements
+base_vote_parlement_legislatives <- read.csv("data/intermediary/parliament/elections and parliament dataset.csv", sep = ",")
+whogov <- read.csv("data/raw/whogov/WhoGov_within_V3.1.csv", sep = ";")
+
