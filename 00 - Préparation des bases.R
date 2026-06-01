39726ee2965dfe739fa1738b2e50d8d7bc7214af
LouisNURDIN <louis.nurdin@etu-iepg.fr>
2026-06-01 09:10:13 +0200

    Add files via upload

---
+++ 00 - Préparation des bases.R
@@ -0,0 +1,114 @@
+library(haven)
+library(dplyr)
+library(tidyr)
+#Ouverture de la base ----
+GMP_DATA <- read_dta("data/raw/wpid/gmp.dta")
+GMP_inc <- read_dta("data/raw/wpid/gmp-inc.dta")
+
+GMP_inc <- GMP_inc %>%
+  rename(dataset_party_id = vote)
+
+sum(GMP_inc$vote == "", na.rm = TRUE)
+sum(GMP_inc$vote == "None", na.rm = TRUE)
+sum(GMP_inc$vote == "No vote", na.rm = TRUE)
+sum(GMP_inc$vote == "Blank", na.rm = TRUE)
+unique(GMP_inc$dinc)  #dinc = décile du répondant
+unique(GMP_inc$type)  # type = type d'élection
+
+sum(GMP_inc$turnout == 1 & GMP_inc$vote == "", na.rm = TRUE)
+sum(GMP_inc$turnout == 0 & GMP_inc$vote != "", na.rm = TRUE) #a priori erreur dans PF, s'en référer à PF
+
+table(GMP_inc$dinc)
+#Ajout Partyfacts avedc id WPID ----
+Partyfacts_id <- read.csv("data/raw/partyfacts-external-parties (1).csv", sep = ";")
+
+Partyfacts_id_wpidmicro <- Partyfacts_id %>%
+  filter(dataset_key == "wpidmicro")
+unique(Partyfacts_id$dataset_key)
+
+Partyfacts_id_parlgov <- Partyfacts_id %>%
+  filter(dataset_key == "parlgov")
+
+
+#2ème méthode = on enlève les élections où turnout = NA + les élections où Turnout = NA et/ou toujours la même valeur----
+GMP_inc_2 <- GMP_inc %>%
+  group_by(isoname, year,type) %>%
+  filter(
+    sum(!is.na(turnout)) > 0,          # pas uniquement NA
+    length(unique(turnout[!is.na(turnout)])) > 1  # au moins 0 ET 1
+  ) %>%
+  ungroup()
+
+#On supprime les électeurs où vote = "vide" et turnout = NA ou 1
+GMP_inc_2 <- GMP_inc_2 %>%
+  filter(
+    !(dataset_party_id == "" & (turnout == 1 | is.na(turnout))))
+
+
+#Abstention dans la 2ème méthode
+GMP_inc_2 <- GMP_inc_2 %>%
+  mutate(
+    dataset_party_id = case_when(
+      dataset_party_id == "Blank" ~ "Abstention",
+      dataset_party_id == "None" ~ "Abstention",
+      dataset_party_id == "No vote" ~ "Abstention", 
+      dataset_party_id == "" & turnout == 0 ~ "Abstention",
+      TRUE ~ dataset_party_id
+    )
+  )
+
+##Ajout iso dans le nom des partis ----
+GMP_inc_2 <- GMP_inc_2 %>%
+  mutate(
+    dataset_party_id = case_when(
+      is.na(dataset_party_id) | dataset_party_id == "" ~ dataset_party_id,
+      TRUE ~ paste0(iso, "-", dataset_party_id)
+    )
+  )
+
+##Join Partyfacts dans GMP inc ----
+GMP_inc_2 <- GMP_inc_2 %>%
+  left_join(
+    Partyfacts_id_wpidmicro %>%
+      dplyr::select(dataset_party_id,partyfacts_id),
+    by = "dataset_party_id"
+  )
+
+library(stringr)
+GMP_inc_2 <- GMP_inc_2 %>%
+  mutate(
+    partyfacts_id = as.character(partyfacts_id),
+    partyfacts_id = case_when(
+      str_detect(dataset_party_id, "Abstention$") ~ "Abstention",
+      str_detect(dataset_party_id, "Other$") ~ "Other",
+      TRUE ~ partyfacts_id
+    )
+  )
+
+
+## Filtre législatives dans deuxième méthode ----
+Base_elections_legislatives <- GMP_inc_2 %>%
+  filter(type == "Lower house")
+unique(GMP_inc$dinc)
+
+#Corrections partis problématiques
+Base_elections_legislatives <- Base_elections_legislatives %>%
+  mutate(
+    partyfacts_id = case_when(
+      partyfacts_id == "1388" & isoname == "United Kingdom" ~ "540",
+      partyfacts_id == "1231" & isoname == "Switzerland" ~ "360",
+      partyfacts_id == "7415" & isoname == "Sweden" ~ "199",
+      partyfacts_id == "5750" & isoname == "Spain" ~ "441",
+      partyfacts_id == "8814" & isoname == "Spain" ~ "441",
+      partyfacts_id == "1004" & isoname == "Canada" & year <= 2003 ~ "232",
+      partyfacts_id == "1004" & isoname == "Canada" & year >= 2003 ~ "1004" ,
+      partyfacts_id == "1044" & isoname == "Finland" ~ "1096" ,
+      partyfacts_id == "4785" & isoname == "France" ~ "1478" ,
+      partyfacts_id == "5514" & isoname == "France" ~ "1083" ,
+      partyfacts_id == "8041" & isoname == "France" ~ "1083" ,
+      TRUE ~ partyfacts_id
+    )
+  )
+
+#Export Base globale et Base législatives
+
---
+++ 02 - Base Parlement.R
@@ -0,0 +1,185 @@
+#Base parlement 
+#Import base élections législatives
+
+
+#Elections global ----
+Data_elections_global <- read.csv ("data/raw/elections global/elections-global-release.csv" , sep = ";")
+
+pays_gmp_legislatives <- unique(base_gmp_inc_deciles_legislatives_2$isoname)
+annees_gmp_legislatives <- unique(base_gmp_inc_deciles_legislatives_2$year)
+
+#filtrer pour avoir que les pays dans WPID
+Data_elections_global_group <- Data_elections_global %>%
+  filter(country_name%in% pays_gmp_legislatives)
+
+Data_elections_global_elections_valides <- Data_elections_global_group %>%
+  group_by(country_name,year) %>%
+  summarise(.groups = "drop")
+
+Data_elections_global_elections_valides <- Data_elections_global_elections_valides %>%
+  rename(isoname = country_name)
+
+check_electionsglobal_wpid_legislatives <- elections_legislatives_valides %>%
+  left_join(
+    Data_elections_global_elections_valides, by = c("isoname", "year")
+  )
+
+check_electionsglobal_wpid_legislatives <- check_electionsglobal_wpid_legislatives %>%
+  filter(check_electionsglobal_wpid_legislatives$year <= 2015)
+
+#Vérifier le nombre d'élections compatibles entre les deux groupes
+intersect(
+  paste(elections_legislatives_valides$isoname, elections_legislatives_valides$year),
+  paste(Data_elections_global_elections_valides$isoname, Data_elections_global_elections_valides$year)
+) %>% length()
+
+#Join entre base élections et base parlement ----
+##Rajouter l'abstention dans elections_global----
+library(dplyr)
+
+abstention_rows <- Data_elections_global %>%
+  group_by(country_name, year, election_date) %>%
+  summarise(
+    seats_total = first(seats_total),
+    .groups = "drop"
+  ) %>%
+  mutate(
+    party = "Abstention",
+    partyfacts_id = "Abstention",
+    seats = 0
+  )
+
+Data_elections_global <- Data_elections_global %>%
+  mutate(partyfacts_id = as.character(partyfacts_id))
+abstention_rows <- abstention_rows %>%
+  mutate(partyfacts_id = as.character(partyfacts_id))
+
+Elections_global <- Data_elections_global %>%
+  bind_rows(abstention_rows) %>%
+  arrange(country_name, year, election_date, party)
+
+Elections_global <- Elections_global %>%
+  rename(isoname = country_name)
+
+Elections_global <- Elections_global %>%
+  semi_join(
+    base_gmp_inc_deciles_legislatives_2 %>%
+      distinct(isoname, year),
+    by = c("isoname", "year")
+  )
+
+##Traitement sur données manquantes ----
+Elections_global <- Elections_global %>%
+  mutate(seats = coalesce(seats, alliance_seats)) %>%
+  group_by(isoname, year, election_date) %>%
+  mutate(
+    seats_total = first(seats_total)
+  ) %>%
+  ungroup()
+
+#Correctifs sur mes bases votes et parlement pour faire join les partis ----
+Elections_global <- Elections_global %>%
+  mutate(
+    partyfacts_id = case_when(
+      party == "other (other-script)" ~ "Other",
+      partyfacts_id == "1246" & isoname == "France" ~ "1083",
+      TRUE ~ partyfacts_id
+    )
+  )
+
+base_gmp_inc_deciles_legislatives_2 <- base_gmp_inc_deciles_legislatives_2 %>%
+  mutate(
+    partyfacts_id = case_when(
+      partyfacts_id == "1388" & isoname == "United Kingdom" ~ "540",
+      partyfacts_id == "1231" & isoname == "Switzerland" ~ "360",
+      partyfacts_id == "7415" & isoname == "Sweden" ~ "199",
+      partyfacts_id == "5750" & isoname == "Spain" ~ "441",
+      partyfacts_id == "8814" & isoname == "Spain" ~ "441",
+      partyfacts_id == "1004" & isoname == "Canada" & year <= 2003 ~ "232",
+      partyfacts_id == "1004" & isoname == "Canada" & year >= 2003 ~ "1004" ,
+      partyfacts_id == "1044" & isoname == "Finland" ~ "1096" ,
+      partyfacts_id == "4785" & isoname == "France" ~ "1478" ,
+      partyfacts_id == "5514" & isoname == "France" ~ "1083" ,
+      partyfacts_id == "8041" & isoname == "France" ~ "1083" ,
+      TRUE ~ partyfacts_id
+    )
+  )
+Elections_global <- Elections_global  %>% 
+  mutate(seats_share = seats / seats_total)
+## Join entre les bases ----
+base_vote_parlement_legislatives <- base_gmp_inc_deciles_legislatives_2 %>%
+  left_join(
+    Elections_global %>%
+      select(isoname, year,partyfacts_id,election_date,seats,seats_total,seats_share),
+    distinct(isoname, year,partyfacts_id),
+    by = c("isoname", "year","partyfacts_id")
+  )
+
+###Traitement pour avoir le taux de députés par partis sur l'ensemble des députés
+
+
+base_vote_parlement_legislatives <- base_vote_parlement_legislatives  %>% 
+  group_by(isoname,year,election_date,decile)%>%
+  mutate(election_couverture_seats = sum(seats_share))
+
+base_vote_parlement_legislatives <- base_vote_parlement_legislatives %>%
+  filter(year <= 2015)
+
+#Lister les partis sans données sur les sièges au parlement ----
+unique(base_vote_parlement_legislatives$vote[is.na(base_vote_parlement_legislatives$seats)])
+
+View(
+  base_vote_parlement_legislatives %>%
+    ungroup() %>%
+    filter(is.na(seats)) %>%
+    distinct(year,isoname,vote,partyfacts_id,seats)
+)
+
+##Liste des élections où le taux de sièges couverts est inférieur à 1----
+View(
+  base_vote_parlement_legislatives %>%
+    ungroup() %>%
+    filter(election_couverture_seats < 1) %>%
+    distinct(year,isoname,election_couverture_seats)
+)   
+
+##Valeurs présentes dans ma base Parlement maps pas élections----
+View(Elections_global %>%
+       distinct(isoname, year, election_date, partyfacts_id,seats,seats_share) %>%
+       anti_join(
+         base_vote_parlement_legislatives %>% distinct(isoname, year, election_date, partyfacts_id),
+         by = c("isoname", "year", "election_date", "partyfacts_id")
+       ))
+
+View(
+  Elections_global %>%
+    filter(seats_share >= 0.05) %>%
+    distinct(isoname, year, election_date, partyfacts_id, seats,seats_share) %>%
+    anti_join(
+      base_vote_parlement_legislatives %>%
+        distinct(isoname, year, election_date, partyfacts_id),
+      by = c("isoname", "year", "election_date", "partyfacts_id")
+    )
+)
+
+table(GMP_inc$religion[GMP_inc$isoname == "France"])
+table(GMP_inc$religious)
+
+#Partis présents dans base élections mais pas Parlement
+View(
+  base_vote_parlement_legislatives %>%
+    ungroup() %>%
+    group_by(isoname, year, partyfacts_id) %>%
+    summarise(
+      mean_pct_votes = mean(pct_votes, na.rm = TRUE),
+      .groups = "drop"
+    ) %>%
+    anti_join(
+      Elections_global %>%
+        distinct(isoname, year, election_date, partyfacts_id),
+      by = c("isoname", "year", "partyfacts_id")
+    )
+)
+
+
+
