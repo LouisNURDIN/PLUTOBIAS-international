b0973202e26466d5cf2eb783d4bad4695a338a9b
Louis Nurdin <nurdin@gael-labo.u-ga.fr>
2026-06-02 09:41:47 +0200

    Join Whogov dans base complète

--- 03 - Base gouvernement.R
+++ 03 - Base gouvernement.R
@@ -1,7 +1,13 @@
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(stringr)
 #Base avec données gouvernements
 base_vote_parlement_legislatives <- read.csv("data/intermediary/parliament/elections and parliament dataset.csv", sep = ",")
 whogov <- read.csv("data/raw/whogov/WhoGov_within_V3.1.csv", sep = ";")
-
+elections_legislatives_valides <- read.csv("data/intermediary/elections/valid legislative elections.csv", sep = ",")
+pays_gmp_legislatives <- unique(elections_legislatives_valides$isoname)
+annees_gmp_legislatives <- unique(elections_legislatives_valides$year)
 #Calcul nombre de ministres par année
 whogov <- whogov %>%
   rename(isoname = country_name)
@@ -24,14 +30,26 @@ whogov_parties <- whogov_parties %>%
 whogov_parties <- whogov_parties %>%
   mutate(partyfacts_id = as.character(partyfacts_id))
 
-base_vote_parlement_legislatives <- base_vote_parlement_legislatives %>%
+
+base_complete <- whogov_parties %>%
   left_join(
-    whogov_parties,
+    base_vote_parlement_legislatives,
     by = c("isoname", "year", "partyfacts_id")
   )
 
+base_complete <- base_complete[!is.na(base_complete$year),]
+
+base_complete <- base_complete %>%
+  select(isoname, year, election_date, survey, decile, partyfacts_id, votes, pct_votes,
+         votes_valides, taux_participation, seats, seats_total, seats_share, ministers_party, total_ministers, ministers_share, election_couverture_seats
+         )
+
+
+
+
+
 #verif nombre de ministres couverts par élection
-base_vote_parlement_legislatives <- base_vote_parlement_legislatives %>%
+base_complete <- base_complete %>%
   mutate(
     ministers_party = coalesce(ministers_party, 0),
     total_ministers = coalesce(total_ministers, 0),
@@ -43,9 +61,14 @@ base_vote_parlement_legislatives <- base_vote_parlement_legislatives %>%
   ) %>%
   ungroup()
 
+#Base avec bonnes élections législatives ----
+base_complete_legislatives <- base_complete %>%
+  filter(isoname%in% pays_gmp_legislatives)
+
+
 #Liste des pays/années avec données incohérentes ----
 View(
-  base_vote_parlement_legislatives %>%
+  base_complete %>%
     ungroup() %>%
     filter(election_couverture_ministers > 1) %>%
     distinct(year,isoname,election_couverture_ministers)
@@ -53,10 +76,12 @@ View(
 
 ##Liste des pays/années où tous les ministres ne sont pas couverts ----
 View(
-  base_vote_parlement_legislatives %>%
+  base_complete %>%
     ungroup() %>%
     filter(election_couverture_ministers < 1) %>%
     distinct(year,isoname,election_couverture_ministers)
 )
+
+
 
 #Avec méthode dinc ----
