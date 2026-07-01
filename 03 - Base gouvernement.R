c6f32228e20fce40af98e803fb7a95c2e06a52eb
Louis Nurdin <nurdin@gael-labo.u-ga.fr>
2026-07-01 14:39:36 +0200

    update chemin d'accès liste élections

--- 03 - Base gouvernement.R
+++ 03 - Base gouvernement.R
@@ -9,7 +9,7 @@ elections_legislatives_valides <- read.csv("data/intermediary/elections/valid el
 elections_legislatives_valides2 <- read.csv("data/intermediary/elections/valid legislative elections.csv", sep = ",")
 pays_gmp_legislatives <- unique(elections_legislatives_valides2$isoname)
 annees_gmp_legislatives <- unique(elections_legislatives_valides2$year)
-all_elections <- read.csv ("data/intermediary/elections/list all elections.csv", sep = ";")
+all_elections <- read.csv ("data/intermediary/elections/all elections update.csv", sep = ";")
 all_elections <- all_elections %>%
   mutate(year = as.numeric(year))
 
